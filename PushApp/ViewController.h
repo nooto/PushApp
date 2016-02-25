8dd311a0a23f0e52386632582b82392a25dec397
GaoAng <gaoang@GaoAngdeiMac.local>
2016-02-25 15:03:28 +0800

    工程添加

---
+++ PushApp/ViewController.h
@@ -0,0 +1,18 @@
+//
+//  ViewController.h
+//  PushApp
+//
+//  Created by GaoAng on 16/2/25.
+//  Copyright © 2016年 GaoAng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+#import "ioSock.h"
+#import <Security/Security.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PushApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PushApp
+//
+//  Created by GaoAng on 16/2/25.
+//  Copyright © 2016年 GaoAng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
